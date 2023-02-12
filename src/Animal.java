b5391f7ac15fbe1893d1894f3cd158c25aca6d80
a1ienforever <artyom.zadvornyj@yandex.ru>
2023-02-12 22:27:51 +0300

    Commit 2

--- src/Animal.java
+++ src/Animal.java
@@ -1,10 +1,29 @@
 public class Animal {
-    public static void main(String[] args) {
-        Animal animal = new Animal();
-        animal.getVoice();
+    private String type;
+    private int age;
+
+    Animal(String type, int age){
+        this.age = age;
+        this.type = type;
     }
 
-     void getVoice(){
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    void getVoice(){
         System.out.println("voice...");
     }
 }
