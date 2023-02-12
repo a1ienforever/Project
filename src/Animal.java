488d00d78aaf97c832fa8ff9ba8259cfd15c5251
a1ienforever <artyom.zadvornyj@yandex.ru>
2023-02-12 22:22:11 +0300

    Commit 1

---
+++ src/Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.getVoice();
+    }
+
+     void getVoice(){
+        System.out.println("voice...");
+    }
+}
