09601d2c9a5e2b07d1a54490142854c22ae81b99
a1ienforever <artyom.zadvornyj@yandex.ru>
2023-02-12 22:32:44 +0300

    Commit 3

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,9 @@
 public class Test {
-
+    public static void main(String[] args) {
+        Animal animal = new Animal("Dog",7);
+        System.out.println("Type: "+ animal.getType()+"\n"+"Age: "+ animal.getAge());
+        animal.setType("mastiff");
+        animal.setAge(8);
+        System.out.println("Type: "+ animal.getType()+"\n"+"Age: "+ animal.getAge());
+    }
 }
